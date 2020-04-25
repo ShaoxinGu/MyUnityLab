4376c61ec8e4902ade1af5ca46773da602b1a1cf
ShaoxinGu <853674765@qq.com>
2020-04-26 01:29:50 +0800

    Add InputMgr & Update EventMgr.

---
+++ Assets/GFramework/Managers/EventMgr.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace GFramework
+{
+    public interface IEventCallBack
+    {
+
+    }
+
+    public class EventCallBack : IEventCallBack
+    {
+        public UnityAction actions;
+        public EventCallBack(UnityAction action)
+        {
+            actions += action;
+        }
+    }
+
+    public class EventCallBack<T> : IEventCallBack
+    {
+        public UnityAction<T> actions;
+        public EventCallBack(UnityAction<T> action)
+        {
+            actions += action;
+        }
+    }
+
+    public class EventMgr : Singleton<EventMgr>
+    {
+        private Dictionary<string, IEventCallBack> eventDic = new Dictionary<string, IEventCallBack>();
+
+        public void AddEventListener(string eventName, UnityAction action)
+        {
+            if (eventDic.ContainsKey(eventName))
+            {
+                (eventDic[eventName] as EventCallBack).actions += action;
+            }
+            else
+            {
+                eventDic.Add(eventName, new EventCallBack(action));
+            }
+        }
+
+        public void AddEventListener<T>(string eventName, UnityAction<T> action)
+        {
+            if (eventDic.ContainsKey(eventName))
+            {
+                (eventDic[eventName] as EventCallBack<T>).actions += action;
+            }
+            else
+            {
+                eventDic.Add(eventName, new EventCallBack<T>(action));
+            }
+        }
+
+        public void RemoveEventListener(string eventName, UnityAction action)
+        {
+            if (eventDic.ContainsKey(eventName))
+                (eventDic[eventName] as EventCallBack).actions -= action;
+        }
+
+        public void RemoveEventListener<T>(string eventName, UnityAction<T> action)
+        {
+            if (eventDic.ContainsKey(eventName))
+                (eventDic[eventName] as EventCallBack<T>).actions -= action;
+        }
+
+        public void DispatchEvent(string eventName)
+        {
+            if (eventDic.ContainsKey(eventName))
+            {
+                if ((eventDic[eventName] as EventCallBack).actions != null)
+                    (eventDic[eventName] as EventCallBack).actions.Invoke();
+            }
+        }
+
+        public void DispatchEvent<T>(string eventName, T param)
+        {
+            if (eventDic.ContainsKey(eventName))
+            {
+                if ((eventDic[eventName] as EventCallBack<T>).actions != null)
+                    (eventDic[eventName] as EventCallBack<T>).actions.Invoke(param);
+            }
+        }
+
+        public void Clear()
+        {
+            eventDic.Clear();
+        }
+    }
+}
---
+++ Assets/GFramework/Managers/InputMgr.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GFramework
+{
+    public class InputMgr : Singleton<InputMgr>
+    {
+        private bool IsCheck = false;
+
+        public InputMgr()
+        {
+            MonoMgr.Instance.AddUpdateListner(InputUpdate);
+        }
+
+        public void SetCheckEnabled(bool isCheck)
+        {
+            IsCheck = isCheck;
+        }
+
+        private void InputUpdate()
+        {
+            if (!IsCheck)
+                return;
+            CheckKeyCode(KeyCode.W);
+            CheckKeyCode(KeyCode.A);
+            CheckKeyCode(KeyCode.S);
+            CheckKeyCode(KeyCode.D);
+        }
+
+        private void CheckKeyCode(KeyCode keyCode)
+        {
+            if (Input.GetKeyDown(keyCode))
+            {
+                EventMgr.Instance.DispatchEvent("KEY_DOWN", keyCode);
+            }
+            if (Input.GetKeyUp(keyCode))
+            {
+                EventMgr.Instance.DispatchEvent("KEY_UP", keyCode);
+            }
+        }
+    }
+}
