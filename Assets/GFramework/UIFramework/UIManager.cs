2d62f68524d785d787202ac2e82073b20ffc99fc
ShaoxinGu <853674765@qq.com>
2020-04-27 22:40:53 +0800

    Update UIManager.cs

--- Assets/GFramework/UIFramework/UIManager.cs
+++ Assets/GFramework/UIFramework/UIManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace GFramework
 {
@@ -25,21 +26,28 @@ namespace GFramework
         public UIManager()
         {
             GameObject obj1 = ResMgr.Instance.Load<GameObject>("UI/Canvas");
-            GameObject.DontDestroyOnLoad(obj1);
+            Object.DontDestroyOnLoad(obj1);
             GameObject obj2 = ResMgr.Instance.Load<GameObject>("UI/EventSystem");
-            GameObject.DontDestroyOnLoad(obj2);
+            Object.DontDestroyOnLoad(obj2);
             bottom = canvas.Find("Bottom");
             midlle = canvas.Find("Middle");
             top = canvas.Find("Top");
             system = canvas.Find("System");
         }
 
-        public void ShowPanel(string name, UI_LAYER layer)
+        public void ShowPanel<T>(string name, UI_LAYER layer, UnityAction<T> callBack = null) where T : BasePanel
         {
-            ResMgr.Instance.LoadAsyn<GameObject>("UI/" + name, (obj)=>
+            if (panelDic.ContainsKey(name))
+            {
+                panelDic[name].Show();
+                callBack?.Invoke(panelDic[name] as T);
+                return;
+            }
+
+            ResMgr.Instance.LoadAsyn<GameObject>("UI/" + name, (obj) =>
             {
                 Transform father = null;
-                switch(layer)
+                switch (layer)
                 {
                     case UI_LAYER.Bottom:
                         father = bottom;
@@ -60,12 +68,22 @@ namespace GFramework
                 obj.transform.localScale = Vector3.one;
                 (obj.transform as RectTransform).offsetMax = Vector3.zero;
                 (obj.transform as RectTransform).offsetMin = Vector3.zero;
+
+                T panel = obj.GetComponent<T>();
+                callBack?.Invoke(panel);
+                panel.Show();
+                panelDic.Add(name, panel);
             });
         }
 
         public void HidePanel(string name)
         {
-
+            if (panelDic.ContainsKey(name))
+            {
+                panelDic[name].Hide();
+                Object.Destroy(panelDic[name].gameObject);
+                panelDic.Remove(name);
+            }
         }
     }
 }
